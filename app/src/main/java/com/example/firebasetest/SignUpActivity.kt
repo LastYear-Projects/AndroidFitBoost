25e5119da5bd3f2bbcd628c96398131bd1d1ae03
Idan Asayag <idanasayag0@gmail.com>
2024-01-13 22:56:26 +0200

    fix the register page

--- app/src/main/java/com/example/firebasetest/SignUpActivity.kt
+++ app/src/main/java/com/example/firebasetest/SignUpActivity.kt
@@ -29,21 +29,27 @@ class SignUpActivity : AppCompatActivity() {
             val email = binding.emailEt.text.toString()
             val password = binding.passET.text.toString()
             val confirmPass = binding.confirmPassEt.text.toString()
+            val phone = binding.phoneEt.text.toString()
+            val regex = Regex("^\\d+\$")
 
-            if(email.isNotEmpty() && password.isNotEmpty() && confirmPass.isNotEmpty()){
-                if(password == confirmPass){
-                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
-                        if(it.isSuccessful){
-                            Toast.makeText(this, "Account created Successfully!", Toast.LENGTH_SHORT).show()
-                            val intent = Intent(this, SignInActivity::class.java)
-                            startActivity(intent)
-                            finish()
-                        }else{
-                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-                        }
-                    }
+            if(email.isNotEmpty() && password.isNotEmpty() && confirmPass.isNotEmpty() && phone.isNotEmpty()){
+                if(!regex.matches(phone)){
+                    Toast.makeText(this, "Phone must be digits only.", Toast.LENGTH_SHORT).show()
                 }else{
-                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+                    if(password == confirmPass){
+                        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                            if(it.isSuccessful){
+                                Toast.makeText(this, "Account created Successfully!", Toast.LENGTH_SHORT).show()
+                                val intent = Intent(this, SignInActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            }else{
+                                Toast.makeText(this, it.exception?.message.toString(), Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }else{
+                        Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }else{
                 Toast.makeText(this, "All the fields are required.", Toast.LENGTH_SHORT).show()
