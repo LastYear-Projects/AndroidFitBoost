b1ad8b546de76a7c5abd3f71ffdffd5529dd91a6
Idan Asayag <idanasayag0@gmail.com>
2024-01-14 20:53:04 +0200

    favorit page -> pick Image from gallery test WORK

--- app/src/main/java/com/example/firebasetest/FavoriteFragment.kt
+++ app/src/main/java/com/example/firebasetest/FavoriteFragment.kt
@@ -1,20 +1,53 @@
 package com.example.firebasetest
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.provider.FontsContractCompat.FontRequestCallback.RESULT_OK
+import com.example.firebasetest.databinding.FragmentFavoriteBinding
+import com.example.firebasetest.databinding.FragmentHomeBinding
 
 
 class FavoriteFragment : Fragment() {
 
+    private lateinit var button: Button
+    private lateinit var imageView: ImageView
+    private lateinit var binding: FragmentFavoriteBinding
+
+    companion object{
+        val IMAGE_REQUEST_CODE = 100
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        var view = inflater.inflate(R.layout.fragment_favorite, container, false)
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        button = binding.btnPickImage
+        imageView = binding.imgSave
+
+        button.setOnClickListener {
+            pickImageGallery()
+        }
+
+        return binding.root
+    }
+
+    private fun pickImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        imageView.setImageURI(data?.data)
     }
 
 }
