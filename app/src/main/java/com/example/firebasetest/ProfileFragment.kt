1071930542db6c59643791c6952f903ca4888ba8
Idan Asayag <idanasayag0@gmail.com>
2024-01-16 22:34:22 +0200

    fix the profile -> add loader

--- app/src/main/java/com/example/firebasetest/ProfileFragment.kt
+++ app/src/main/java/com/example/firebasetest/ProfileFragment.kt
@@ -75,7 +75,55 @@ class ProfileFragment : Fragment() {
         }
     }
 
+    private fun checkVisible(){
+        if(binding.loader.visibility == View.VISIBLE){
+            binding.tvProfileName.visibility = View.GONE
+            binding.tvProfilePhone.visibility = View.GONE
+            binding.tvProfileEmail.visibility = View.GONE
+            binding.tvProfileWeight.visibility = View.GONE
+            binding.tvProfileHeight.visibility = View.GONE
+            binding.tvProfileGender.visibility = View.GONE
+            binding.tvProfileAge.visibility = View.GONE
+            binding.imageView.visibility = View.GONE
+            binding.textView16.visibility = View.GONE
+            binding.textView2.visibility = View.GONE
+            binding.textView20.visibility = View.GONE
+            binding.textView22.visibility = View.GONE
+            binding.textView24.visibility = View.GONE
+            binding.textView26.visibility = View.GONE
+            binding.textView27.visibility = View.GONE
+            binding.textView29.visibility = View.GONE
+            binding.btnCM.visibility = View.GONE
+            binding.btnKG.visibility = View.GONE
+            binding.btnEdit.visibility = View.GONE
+        }else{
+            binding.tvProfileName.visibility = View.VISIBLE
+            binding.tvProfilePhone.visibility = View.VISIBLE
+            binding.tvProfileEmail.visibility = View.VISIBLE
+            binding.tvProfileWeight.visibility = View.VISIBLE
+            binding.tvProfileHeight.visibility = View.VISIBLE
+            binding.tvProfileGender.visibility = View.VISIBLE
+            binding.tvProfileAge.visibility = View.VISIBLE
+            binding.imageView.visibility = View.VISIBLE
+            binding.textView16.visibility = View.VISIBLE
+            binding.textView2.visibility = View.VISIBLE
+            binding.textView20.visibility = View.VISIBLE
+            binding.textView22.visibility = View.VISIBLE
+            binding.textView24.visibility = View.VISIBLE
+            binding.textView26.visibility = View.VISIBLE
+            binding.textView27.visibility = View.VISIBLE
+            binding.textView29.visibility = View.VISIBLE
+            binding.btnCM.visibility = View.VISIBLE
+            binding.btnKG.visibility = View.VISIBLE
+            binding.btnEdit.visibility = View.VISIBLE
+        }
+    }
+
     private fun fetchDataAndPopulateUI() {
+        binding.loader.visibility = View.VISIBLE
+        checkVisible()
+
+
         // Fetch the current user data from the ViewModel
         mUserViewModel.getCurrentUserLiveData().observe(viewLifecycleOwner, { currentUser ->
             // Update UI with the current user's data
@@ -86,6 +134,8 @@ class ProfileFragment : Fragment() {
 //            binding.tvProfileHeight.text = createEditable(currentUser.height)
 //            binding.tvProfileGender.text = createEditable(currentUser.gender)
 //            binding.tvProfileAge.text = createEditable(currentUser.age)
+            binding.loader.visibility = View.GONE
+            checkVisible()
         })
     }
 
