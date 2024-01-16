c110b49232aa4f7d1251c79b559f80ef85081144
DanDavidovich <davidovich.dan@gmail.com>
2024-01-16 13:21:57 +0200

    rmv button

--- app/src/main/java/com/example/firebasetest/ExeceriseAdapter.kt
+++ app/src/main/java/com/example/firebasetest/ExeceriseAdapter.kt
@@ -3,16 +3,21 @@ package com.example.firebasetest
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 
-class ExeceriseAdapter(private val execeriseList: ArrayList<Execerise>)
+class ExeceriseAdapter(
+    private val execeriseList: ArrayList<Execerise>,
+    private val onItemRemoved: () -> Unit)
     : RecyclerView.Adapter<ExeceriseAdapter.ExeceriseViewHolder>() {
 
         class ExeceriseViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
             val name: TextView = itemView.findViewById(R.id.execeriseNameTv)
             val reps: TextView = itemView.findViewById(R.id.execeriseRepsTv)
             val sets: TextView = itemView.findViewById(R.id.execeriseSetsTv)
+            val removeButton: Button = itemView.findViewById(R.id.btnRemoveExercise)
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExeceriseViewHolder {
@@ -30,5 +35,16 @@ class ExeceriseAdapter(private val execeriseList: ArrayList<Execerise>)
         holder.reps.text = execerise.reps
         holder.sets.text = execerise.sets
 
+        holder.removeButton.setOnClickListener {
+            // Remove the item from the list
+            removeItem(position)
+            holder.let { it1 -> Snackbar.make(it, "Item was removed", Snackbar.LENGTH_LONG).show() }
+        }
+
+    }
+    private fun removeItem(position: Int) {
+        execeriseList.removeAt(position)
+        notifyItemRemoved(position)
+        onItemRemoved()
     }
 }
