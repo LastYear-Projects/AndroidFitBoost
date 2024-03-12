4ef1565ff698d37ae19925896f378782ec8e3615
DanDavidovich <davidovich.dan@gmail.com>
2024-03-12 21:16:35 +0200

    refactor names

--- app/src/main/java/com/example/firebasetest/MyWorkoutsActivity.kt
+++ app/src/main/java/com/example/firebasetest/MyWorkoutsActivity.kt
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 class MyWorkoutsActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var workoutList: ArrayList<Workout>
-    private lateinit var gymAdapter: WorkoutAdapter
+    private lateinit var workoutAdapter: WorkoutAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_workouts2)
