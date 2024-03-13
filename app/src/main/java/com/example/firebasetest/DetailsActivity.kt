88193e746e0c42ca4dd86a65bc0b946154867a62
DanDavidovich <davidovich.dan@gmail.com>
2024-03-13 15:32:39 +0200

    added firestore

--- app/src/main/java/com/example/firebasetest/DetailsActivity.kt
+++ app/src/main/java/com/example/firebasetest/DetailsActivity.kt
@@ -2,17 +2,25 @@ package com.example.firebasetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.picasso.Picasso
 
 class DetailsActivity : AppCompatActivity() {
+    private lateinit var firestore: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detailed)
 
         val gym = intent.getParcelableExtra<Workout>("gym")
+
+        firestore = FirebaseFirestore.getInstance()
+
         if(gym != null){
             val textView: TextView = findViewById(R.id.detailedActivityTv)
             val imageView: ImageView = findViewById(R.id.detailedActivityIv)
