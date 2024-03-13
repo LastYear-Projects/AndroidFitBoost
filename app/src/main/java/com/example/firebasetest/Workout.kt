c6455234b138f008a761775ebb5a4d7f854b033e
DanDavidovich <davidovich.dan@gmail.com>
2024-03-13 12:41:56 +0200

    update the workout to include owner , duration and exercises

--- app/src/main/java/com/example/firebasetest/Workout.kt
+++ app/src/main/java/com/example/firebasetest/Workout.kt
@@ -3,11 +3,21 @@ package com.example.firebasetest
 import android.os.Parcel
 import android.os.Parcelable
 
-data class Workout(val image: String, val name: String, val subtitle: String) : Parcelable {
+data class Workout(
+    val image: String, val name: String, val subtitle: String,
+    val duration: String,
+    val owner:String,
+    val exercises: ArrayList<*>
+)  : Parcelable {
     constructor(parcel: Parcel) : this(
         parcel.readString()!!,
         parcel.readString()!!,
-        parcel.readString()!!
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        arrayListOf<Execerise>().apply {
+            parcel.readList(this as List<*>,Execerise::class.java.classLoader)
+        }
     )
 
     override fun describeContents(): Int {
@@ -18,6 +28,10 @@ data class Workout(val image: String, val name: String, val subtitle: String) : 
         dest.writeString(image)
         dest.writeString(name)
         dest.writeString(subtitle)
+        dest.writeString(duration)
+        dest.writeString(owner)
+        dest.writeList(exercises as List<*>?)
+
     }
 
     companion object CREATOR : Parcelable.Creator<Workout> {
